d70608aebff2ada22d9f0a02b0ea5a6372b90e71
伊藤友恒 <tomoito@itoutomohisashinoMacBook-Air.local>
2021-02-24 08:47:16 +0900

    commitQ

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,13 +1,16 @@
-import Layout from '@/components/organisms/Layout/Layout'
-import React from 'react'
+import Layout from '@/components/organisms/Layout/Layout';
+import React from 'react';
 
 const contact = () => {
-    return (
-<Layout>
-    <h1>Contact me</h1>
-    <h1>tomotun1210@gmail.com</h1>
-</Layout>
-    )
-}
+  return (
+    <Layout>
+      <h1>Contact me</h1>
+      <h1>tomotun1210@gmail.com</h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+      </button>
+    </Layout>
+  );
+};
 
-export default contact
+export default contact;
