dd5d0f49660f4cc77c031f3dffea877f3b261d3a
伊藤友恒 <tomoito@itoutomohisashinoMacBook-Air.local>
2021-02-24 14:29:45 +0900

    commit

--- src/components/templates/Top/Top.tsx
+++ src/components/templates/Top/Top.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './styles.module.css';
 import Image from 'next/image';
+import { About } from '../About/About';
 
 const Top = () => {
   return (
-    <main className={styles.profile}>
-      <h2 className={styles.title}>Ok This is me!!</h2>
-      <Image src="/images/profile2.jpg" alt="Picture of the author" width={600} height={600} />
-    </main>
+    // <main className={styles.profile}>
+    //   <h2 className={styles.title}>Ok This is me!!</h2>
+    //   <Image src="/images/profile2.jpg" alt="Picture of the author" width={600} height={600} />
+    // </main>
+    <About />
   );
 };
 
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -4,11 +4,13 @@ import React from 'react';
 const contact = () => {
   return (
     <Layout>
-      <h1>Contact me</h1>
-      <h1>tomotun1210@gmail.com</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Button
-      </button>
+      {/* <h1>Contact me</h1> */}
+      <main>
+        <h1>tomotun1210@gmail.com</h1>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Button
+        </button>
+      </main>
     </Layout>
   );
 };
