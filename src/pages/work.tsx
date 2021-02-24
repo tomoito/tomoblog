5241918854d713a4b0e7fbd0cedfb13cafabded1
伊藤友恒 <tomoito@itoutomohisashinoMacBook-Air.local>
2021-02-24 21:06:41 +0900

    comit

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './styles.module.css';
 import Layout from '@/components/organisms/Layout/Layout';
-import { Work } from '@/components/templates/Work/work';
 import { Art } from '@/components/templates/Work/Art';
 
 const Page = () => (
