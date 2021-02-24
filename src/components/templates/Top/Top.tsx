0a17d5c3668dc8d700fa5c3c54003f02850cb779
伊藤友恒 <tomoito@itoutomohisashinoMacBook-Air.local>
2021-02-24 09:14:06 +0900

    commit

--- src/components/templates/Top/Top.tsx
+++ src/components/templates/Top/Top.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const Top = () => {
   return (
     <main className={styles.profile}>
-      <h2 className={styles.title}>This is me!!</h2>
+      <h2 className={styles.title}>Ok This is me!!</h2>
       <Image src="/images/profile2.jpg" alt="Picture of the author" width={600} height={600} />
     </main>
   );
