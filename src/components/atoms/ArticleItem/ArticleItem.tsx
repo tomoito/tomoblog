5e36bde5a629fa40723f7df8cb1ae98dedb6c75c
伊藤友恒 <tomoito@itoutomohisashinoMacBook-Air.local>
2021-02-24 17:07:52 +0900

    commit

--- src/components/atoms/ArticleItem/ArticleItem.tsx
+++ src/components/atoms/ArticleItem/ArticleItem.tsx
@@ -16,7 +16,7 @@ const ArticleItem = ({ article }) => {
             href="#"
             className="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500"
           >
-            {article.title}
+            {article.id}
           </a>
         </div>
         <div className="mt-2">
