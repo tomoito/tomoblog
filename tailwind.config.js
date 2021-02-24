3ca3efb82ce6b43a57718d70ecc2244e18b41fae
伊藤友恒 <tomoito@itoutomohisashinoMacBook-Air.local>
2021-02-24 09:22:38 +0900

    m

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ['./components/**/*.tsx', './pages/**/*.tsx', './public/**/*.html'],
+  // purge: ['./components/**/*.tsx', './pages/**/*.tsx', './public/**/*.html'],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
